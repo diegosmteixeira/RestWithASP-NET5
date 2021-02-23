3ee83926864e8ef7aaf56bbccc38def45e7315ef
diegosmteixeira <diegosmteixeira@gmail.com>
2021-02-23 15:29:44 -0300

    Working with HTTP Verbs mocking person queries

---
+++ 03_RestWithASPNET_Person/RestWithASPNET/RestWithASPNET/Services/Implementations/IPersonService.cs
@@ -0,0 +1,19 @@
+﻿using RestWithASPNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
