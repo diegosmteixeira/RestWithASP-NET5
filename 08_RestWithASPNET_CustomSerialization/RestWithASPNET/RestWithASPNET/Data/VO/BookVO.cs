cf51d8da81959ec77b56ad30e1e5d2e71afcd08a
diegosmteixeira <diegosmteixeira@gmail.com>
2021-03-04 12:26:54 -0300

    Custom Serialization

---
+++ 08_RestWithASPNET_CustomSerialization/RestWithASPNET/RestWithASPNET/Data/VO/BookVO.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNET.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Model
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LauchDate { get; set; }
+    }
+}
---
+++ 08_RestWithASPNET_CustomSerialization/RestWithASPNET/RestWithASPNET/Data/VO/PersonVO.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNET.Data.VO
+{
+    public class PersonVO
+    {
+        //when use POST and PUT = persistence will be with customization's name.
+
+        [JsonPropertyName ("code")] //it's just a customization
+        public long Id { get; set; }
+
+        [JsonPropertyName("first_name")] //it's just a customization
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")] //it's just a customization
+        public string LastName { get; set; }
+
+        [JsonIgnore]
+        public string Address { get; set; }
+
+        [JsonPropertyName("sex")] //it's just a customization
+        public string Gender { get; set; }
+    }
+}
