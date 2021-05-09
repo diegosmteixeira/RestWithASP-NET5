94958cfb3358b75565e88df3389deab0fc80450a
diegosmteixeira <diegosmteixeira@gmail.com>
2021-05-09 08:45:41 -0300

    Working with HttpPatch

---
+++ 14_RestWithASPNET_Patch/RestWithASPNET/RestWithASPNET/Repository/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNET.Data.VO;
+using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Repository
+{
+    public interface IPersonRepository : IRepository<Person>
+    {
+        Person Disable(long id);
+    }
+}
